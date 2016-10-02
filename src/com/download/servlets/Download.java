5c58bf8abf68c71db40c085bb4dcabe1e6661bca
sgstyjy <feizhangchina@gmail.com>
2016-10-02 22:13:27 +0200

    download

--- src/com/download/servlets/Download.java
+++ src/com/download/servlets/Download.java
@@ -1,8 +1,12 @@
 package com.download.servlets;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -29,32 +33,47 @@ public class Download extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		// Set the response message's MIME type
-	      response.setContentType("text/html;charset=UTF-8");
-	      // Allocate a output writer to write the response message into the network socket
-	      PrintWriter out = response.getWriter();
-	 
-	      // Write the response message, in an HTML page
-	      try {
-	         out.println("<!DOCTYPE html>");
-	         out.println("<html><head>");
-	         out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
-	         out.println("<title>Hello, World</title></head>");
-	         out.println("<body>");
-	         out.println("<h1>Hello, world!</h1>");  // says Hello
-	         // Echo client's request information
-	         out.println("<p>Request URI: " + request.getRequestURI() + "</p>");
-	         out.println("<p>Protocol: " + request.getProtocol() + "</p>");
-	         out.println("<p>PathInfo: " + request.getPathInfo() + "</p>");
-	         out.println("<p>Remote Address: " + request.getRemoteAddr() + "</p>");
-	         // Generate a random number upon each request
-	         out.println("<p>A Random Number: <strong>" + Math.random() + "</strong></p>");
-	         out.println("</body>");
-	         out.println("</html>");
-	      } finally {
-	         out.close();  // Always close the output writer
-	      }
+		// reads input file from an absolute path
+        String filePath = "download.txt";
+        File downloadFile = new File(filePath);
+        FileInputStream inStream = new FileInputStream(downloadFile);
+         
+        // if you want to use a relative path to context root:
+        String relativePath = getServletContext().getRealPath("");
+        System.out.println("relativePath = " + relativePath);
+         
+        // obtains ServletContext
+        ServletContext context = getServletContext();
+         
+        // gets MIME type of the file
+        String mimeType = context.getMimeType(filePath);
+        if (mimeType == null) {        
+            // set to binary type if MIME mapping not found
+            mimeType = "application/octet-stream";
+        }
+        System.out.println("MIME type: " + mimeType);
+         
+        // modifies response
+        response.setContentType(mimeType);
+        response.setContentLength((int) downloadFile.length());
+         
+        // forces download
+        String headerKey = "Content-Disposition";
+        String headerValue = String.format("attachment; filename=\"%s\"", downloadFile.getName());
+        response.setHeader(headerKey, headerValue);
+         
+        // obtains response's output stream
+        OutputStream outStream = response.getOutputStream();
+         
+        byte[] buffer = new byte[4096];
+        int bytesRead = -1;
+         
+        while ((bytesRead = inStream.read(buffer)) != -1) {
+            outStream.write(buffer, 0, bytesRead);
+        }
+         
+        inStream.close();
+        outStream.close();     
 	}
 
 	/**
